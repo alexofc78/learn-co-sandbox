292563040eb0acafcdd0cf1a36644d80c3b7c0db
ALEXANDRA OLIVEROS <alexofc@gmail.com>
2020-08-20 18:46:41 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,6 @@
+# From the Three Dog Night song "Joy to the world (Jeremiah was a Bullfrog)"
+puts "Joy to the world"
+puts "All the boys and girls"
+puts "Joy to the fishes in the deep blue sea"
+puts "Joy to you and me"
+#how to switch names from Luca to Byron
